1678b5fb238e10a9c32d6f5b65c28c16ebf1c873
Prince <avecillasprince@yahoo.com>
2021-08-05 11:23:18 -0400

    QWE

--- task-manager/src/Header.js
+++ task-manager/src/Header.js
@@ -4,21 +4,26 @@ import { NavLink } from "react-router-dom"
  function Header () { 
     return (
         <body>
-            <nav>
+            <nav className="head">
                 <label className="title">☞ TaskMaster ☜</label>
+                
+            </nav>
+            <nav className="options">
                 <ul class="nav">
-                    <li><NavLink exact to="/">Home</NavLink></li>
-                    <li><NavLink to="/todo">To-Do</NavLink></li>
-                    <li><NavLink to="/groceries">Groceries</NavLink></li>
-                    <li><NavLink to="/movies">Watchlist</NavLink>
-                        <ul>
-                            <li class="dropdown"><NavLink to="/movieadd">Add Movie</NavLink></li>
-                        </ul>
-                    </li>
-                    <li><NavLink to="/misc">Misc</NavLink></li>
+                        <li><NavLink exact to="/">Home</NavLink></li>
+                        <li><NavLink to="/todo">To-Do</NavLink></li>
+                        <li><NavLink to="/groceries">Groceries</NavLink></li>
+                        <li><NavLink to="/movies">Watchlist</NavLink>
+                            <ul>
+                                <li class="dropdown"><NavLink to="/movieadd">Add Movie</NavLink></li>
+                            </ul>
+                        </li>
+                        <li><NavLink to="/misc">Misc</NavLink></li>
                 </ul>
             </nav>
         </body>
+        
+        
     )
 }
 
